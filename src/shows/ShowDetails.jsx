11e25a531106a797c198151b927a01ec8e0d5779
AOrnstein <ornstein.adam@gmail.com>
2026-05-12 11:19:36 -0600

    Implemented ShowDetails component

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
 import "./shows.css";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(show);
+
+  // If no show is provided, render a message asking the user to select a show
+  if (!selectedEpisode)
+    return (
+      <div className="show-details">
+        <p>Please select a show</p>
+      </div>
+    );
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
